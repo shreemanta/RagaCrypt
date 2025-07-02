727568aaf0810cb57b5fc71a929b624f5a9b32c6
shreemanta <142534011+shreemanta@users.noreply.github.com>
2025-07-02 15:37:43 +0530

    Background

--- Raga/src/Pages/Home.jsx
+++ Raga/src/Pages/Home.jsx
@@ -3,12 +3,14 @@ import './Home.css'
 
 function Home() {
   return (
+    <div className="main">
     <div className="parent">
       <div className="HeadMaster">
         <h1 className='wel'>Welcome to रागाCrypt</h1>
         <span className='slogan'>Explore Cipher , Experience Security</span>
       </div>
     </div>
+     </div>
   )
 }
 
